c22113f3de638d007482d12623f6ce86c13f01df
parkj <parkj@mbproparkj.local>
2022-04-14 00:13:44 -0700

    bug

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 from datetime import timedelta
 
-from security import authenticate, identity # as identity_function
+from security import authenticate, identity as identity_function
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList # sqlAlchemy needs this to create tables from the store.py file
@@ -19,26 +19,26 @@ api = Api(app)
 
 app.config['JWT_AUTH_URL_RULE'] = '/login' # optional code that changes /auth to /login ** MUST BE PLACED BEFORE requesting the JWT instance
 
-jwt = JWT(app, authenticate, identity) #_function) # /auth
+jwt = JWT(app, authenticate, identity_function) # /auth
 
 # optional include user's Id in the response in addition to access_token
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity):
-    return jsonify({
-        'access_token': access_token.decode('utf-8'),
-        'user_id': identity.id
-    })
+# @jwt.auth_response_handler
+# def customized_response_handler(access_token, identity):
+#     return jsonify({
+#         'access_token': access_token.decode('utf-8'),
+#         'user_id': identity.id
+#     })
 
 # optional Flask-JWT raises JWTError when an error occurs within any of the handlers
-@jwt.error_handler
-def customized_error_handler(error):
-    return jsonify({
-        'message': error.description,
-        'code': error.status_code
-    }), error.status_code
+# @jwt.error_handler
+# def customized_error_handler(error):
+#     return jsonify({
+#         'message': error.description,
+#         'code': error.status_code
+#     }), error.status_code
 
 # config JWT to expire within half hour
-app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+# app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
 # config JWT auth key name to be 'email' instead of default 'username'
 app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
