ab4358de115ddb9924b380f1960e052d6cfc074c
parkj <parkj@mbproparkj.local>
2022-04-14 00:11:19 -0700

    Adding back commented out code

--- app.py
+++ app.py
@@ -17,28 +17,28 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'josh'
 api = Api(app)
 
-# app.config['JWT_AUTH_URL_RULE'] = '/login' # optional code that changes /auth to /login ** MUST BE PLACED BEFORE requesting the JWT instance
+app.config['JWT_AUTH_URL_RULE'] = '/login' # optional code that changes /auth to /login ** MUST BE PLACED BEFORE requesting the JWT instance
 
 jwt = JWT(app, authenticate, identity) #_function) # /auth
 
 # optional include user's Id in the response in addition to access_token
-# @jwt.auth_response_handler
-# def customized_response_handler(access_token, identity):
-#     return jsonify({
-#         'access_token': access_token.decode('utf-8'),
-#         'user_id': identity.id
-#     })
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+        'access_token': access_token.decode('utf-8'),
+        'user_id': identity.id
+    })
 
 # optional Flask-JWT raises JWTError when an error occurs within any of the handlers
-# @jwt.error_handler
-# def customized_error_handler(error):
-#     return jsonify({
-#         'message': error.description,
-#         'code': error.status_code
-#     }), error.status_code
+@jwt.error_handler
+def customized_error_handler(error):
+    return jsonify({
+        'message': error.description,
+        'code': error.status_code
+    }), error.status_code
 
 # config JWT to expire within half hour
-# app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 
 # config JWT auth key name to be 'email' instead of default 'username'
 app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
